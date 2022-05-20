8375dcb6c9d32dcccb2704fd0a4713cdbf9d8741
Bronislav Shevelev <shevelevbv@hotmail.com>
2022-05-20 14:01:55 -0400

    feat: add SIGINT event to 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -11,13 +11,15 @@ const {stdin, stdout} = process;
 const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 const rl = readline.createInterface(stdin, stdout);
 
-rl.setPrompt('Hi! Please enter some text here:\n');
+rl.setPrompt('Hi! Please enter some text here (enter \'exit\' or press Ctrl+C to exit):\n');
 rl.prompt();
 
 rl.on('line', line => {
   if (line.trim() === 'exit') process.exit();
   output.write(line + '\n');
 });
+
+rl.on('SIGINT', () => process.exit());
 rl.on('error', error => console.error(error.message));
 
 process.on('exit', () => stdout.write('Have a nice day!\n'));
